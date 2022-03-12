1831b38318c0e938366610d720623a3d5889a900
Angelica <Angelica137@users.noreply.github.com>
2022-03-12 21:23:19 +0000

    add replace_word_choice(), green

--- scripts/string_methods.py
+++ scripts/string_methods.py
@@ -8,3 +8,11 @@ def check_sentence_ending(sentence: str) -> bool:
 
 def clean_up_spacing(sentence: str) -> str:
     return sentence.strip()
+
+
+def replace_word_choice(sentence: str, old_word: str, new_word: str) -> str:
+    """
+    Replaces :param: old_word with :param: new_word and
+    returns new string
+    """
+    return sentence.replace(old_word, new_word)
