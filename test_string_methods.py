8678e9e6876a27b8697c5546ebad942f0d4165f9
Angelica <Angelica137@users.noreply.github.com>
2022-03-12 21:04:44 +0000

    test check_sentence_ending(sentence), red

--- test_string_methods.py
+++ test_string_methods.py
@@ -3,3 +3,8 @@ from scripts.string_methods import *
 
 def test_capitalize_title():
     assert capitalize_title("my hobbies") == "My Hobbies"
+
+
+def test_check_sentence_ending():
+    sentence = "and read."
+    assert check_sentence_ending(sentence) == True
